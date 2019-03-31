73eb604721d47afbbd26c6c6ba44df8dd2b57d50
dani97 <christopherdaniel13397@gmail.com>
2019-03-31 08:46:45 +0530

    GSAP

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Description
+
+This is a example app integrating GSAP with react.
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
+import React, {useEffect, useRef} from 'react';
+import {TweenMax, Linear} from 'gsap';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () => {
+    let logoElement = useRef(null);
+
+
+
+    useEffect(() => {
+        TweenMax.to(
+            logoElement,
+            1,
+            {
+                repeat: -1,
+                rotation: 360,
+                ease: Linear.easeNone
+            }
+        )
+    }, []);
+
+    function scaleUp() {
+        TweenMax.to(logoElement, 1, {
+            scale: 1.25,
+            ease: Linear.ease
+        });
+    }
+
+    function scaleDown() {
+        TweenMax.to(logoElement, 1, {
+            scale: 0.75
+        });
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} onMouseEnter={scaleUp} onMouseLeave={scaleDown} ref={element => {logoElement = element}} className="App-logo" alt="logo"/>
+                <p>
+                   React + GSAP animation
+                </p>
+            </header>
+        </div>
     );
-  }
 }
+
 
 export default App;
